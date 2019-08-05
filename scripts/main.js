bd7b2f352f3f65598085957ed4fedc10cb068ac8
timwong00 <wongtime317@gmail.com>
2019-08-05 08:17:37 -0400

    added contact form

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,4 @@
-// const gitLink = "https://timwong00.github.io";
-// const routes = {
-//   "/": landing,
-//   "/projects": projects,
-//   "/contact": contact
-// };
+"use strict";
 
-// const contentContainer = document.getElementById("contentContainer");
-
-// const onNavigate = pathname => {
-//   window.history.pushState({}, pathname, window.location.origin + pathname);
-//   contentContainer.innerHTML = routes[pathname];
-// };
-
-// window.onpopstate = () => {
-//   contentContainer.innerHTML = routes[window.location.pathname];
-// };
+let pageTitle = document.querySelector("#pageTitle");
+console.log(pageTitle);
