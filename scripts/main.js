c263979c51e38508654c90323b1c599f3e85d174
timwong00 <wongtime317@gmail.com>
2019-08-06 14:19:24 -0400

    added project carousel

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,33 @@
 "use strict";
 
-let pageTitle = document.querySelector("#pageTitle");
-console.log(pageTitle);
+// Project Carousel
+let projectIndex = 1;
+displayProject(projectIndex);
+
+function nextProject(num) {
+  displayProject((projectIndex += num));
+}
+
+function currentProject(num) {
+  displayProject((projectIndex = num));
+}
+
+function displayProject(num) {
+  let i;
+  let projects = document.querySelectorAll(".projectItem");
+  let dots = document.querySelectorAll(".dot");
+  if (num > projects.length) {
+    projectIndex = 1;
+  }
+  if (num < 1) {
+    projectIndex = projects.length;
+  }
+  for (i = 0; i < projects.length; i++) {
+    projects[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  projects[projectIndex - 1].style.display = "flex";
+  dots[projectIndex - 1].className += " active";
+}
